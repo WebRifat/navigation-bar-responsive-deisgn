3d57bdc03f884ce391b4d15a676ea7194e9ae822
MD. Rifatul Islam <104375987+WebRifat@users.noreply.github.com>
2024-04-26 12:14:29 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let menubutton = document.querySelector("nav .fa.fa-bars");
+let menus = document.querySelector("nav .menu");
+let nav = document.querySelector("nav");
+
+
+
+function menubtn () {
+
+    nav.classList.toggle("active");
+
+}
+    
+
+
+
+
+
+menubutton.addEventListener("click", menubtn);
